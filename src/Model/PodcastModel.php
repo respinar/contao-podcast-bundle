08f16a2399c43c0e0e1f41088b9f66025d8092e7
Hamid Paywasti <abbaszadeh.h@gmail.com>
2023-06-04 19:47:50 +0330

    Add Models

---
+++ src/Model/PodcastModel.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao Simple Podcast.
+ *
+ * (c) Hamid Abbaszadeh 2023 <abbaszadeh.h@gmail.com>
+ * @license MIT
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ * @link https://github.com/respinar/contao-podcast-bundle
+ */
+
+namespace Respinar\ContaoPodcastBundle\Model;
+
+use Contao\Model;
+
+class PodcastModel extends Model
+{
+    protected static $strTable = 'tl_podcast';
+}
