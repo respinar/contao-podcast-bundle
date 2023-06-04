469eba20e5470787a5fec0e9c5fdb60462200785
Hamid Paywasti <abbaszadeh.h@gmail.com>
2023-06-04 19:45:53 +0330

    Add backend modules

---
+++ contao/dca/tl_podcast.php
@@ -0,0 +1,195 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao Simple Podcast.
+ *
+ * (c) Hamid Abbaszadeh 2023 <abbaszadeh.h@gmail.com>
+ * @license MIT
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ * @link https://github.com/respinar/contao-podcast-bundle
+ */
+
+use Contao\Backend;
+use Contao\DataContainer;
+use Contao\DC_Table;
+use Contao\Input;
+
+/**
+ * Table tl_podcast
+ */
+$GLOBALS['TL_DCA']['tl_podcast'] = array(
+    'config'      => array(
+        'dataContainer'    => DC_Table::class,
+        'c_table'          => array('tl_podcast_episode'),
+        'enableVersioning' => true,
+        'switchToEdit'     => true,
+        'markAsCopy'       => 'title',
+        'sql'              => array(
+            'keys' => array(
+                'id' => 'primary'
+            )
+        ),
+    ),
+    'list'        => array(
+        'sorting'           => array(
+            'mode'        => DataContainer::MODE_SORTABLE,
+            'fields'      => array('title'),
+            'flag'        => DataContainer::SORT_INITIAL_LETTER_ASC,
+            'panelLayout' => 'filter;search,limit'
+        ),
+        'label'             => array(
+            'fields' => array('title'),
+            'format' => '%s',
+        ),
+        'global_operations' => array(
+            'all' => array(
+                'href'       => 'act=select',
+                'class'      => 'header_edit_all',
+                'attributes' => 'onclick="Backend.getScrollOffset()" accesskey="e"'
+            )
+        ),
+        'operations'        => array(
+            'edit'   => array(
+                'href'  => 'table=tl_podcast_episode',
+                'icon'  => 'edit.svg'
+            ),
+            'editheader'   => array(
+                'href'  => 'act=edit',
+                'icon'  => 'header.svg'
+            ),
+            'copy'   => array(
+                'href'  => 'act=copy',
+                'icon'  => 'copy.svg'
+            ),
+            'delete' => array(
+                'href'       => 'act=delete',
+                'icon'       => 'delete.svg',
+                'attributes' => 'onclick="if(!confirm(\'' . ($GLOBALS['TL_LANG']['MSC']['deleteConfirm'] ?? null) . '\'))return false;Backend.getScrollOffset()"'
+            ),
+            'show'   => array(
+                'href'       => 'act=show',
+                'icon'       => 'show.svg',
+                'attributes' => 'style="margin-right:3px"'
+            ),
+        )
+    ),
+
+    // Palettes
+    'palettes' => array
+	(
+		'__selector__'                => array('protected', 'allowComments'),
+		'default'                     => '{title_legend},title,jumpTo;{protected_legend:hide},protected;{comments_legend:hide},allowComments'
+	),
+
+	// Subpalettes
+	'subpalettes' => array
+	(
+		'protected'                   => 'groups',
+		'allowComments'               => 'notify,sortOrder,perPage,moderate,bbcode,requireLogin,disableCaptcha'
+	),
+    'fields'      => array(
+        'id'             => array(
+            'sql' => "int(10) unsigned NOT NULL auto_increment"
+        ),
+        'tstamp'         => array(
+            'sql' => "int(10) unsigned NOT NULL default '0'"
+        ),
+        'title'          => array(
+            'exclude'   => true,
+			'search'    => true,
+			'inputType' => 'text',
+			'eval'      => array('mandatory'=>true, 'maxlength'=>255, 'tl_class'=>'w50'),
+			'sql'       => "varchar(255) NOT NULL default ''"
+        ),
+        'jumpTo' => array
+		(
+			'exclude'                 => true,
+			'inputType'               => 'pageTree',
+			'foreignKey'              => 'tl_page.title',
+			'eval'                    => array('mandatory'=>true, 'fieldType'=>'radio', 'tl_class'=>'clr'),
+			'sql'                     => "int(10) unsigned NOT NULL default 0",
+			'relation'                => array('type'=>'hasOne', 'load'=>'lazy')
+		),
+		'protected' => array
+		(
+			'exclude'                 => true,
+			'filter'                  => true,
+			'inputType'               => 'checkbox',
+			'eval'                    => array('submitOnChange'=>true),
+			'sql'                     => "char(1) NOT NULL default ''"
+		),
+		'groups' => array
+		(
+			'exclude'                 => true,
+			'inputType'               => 'checkbox',
+			'foreignKey'              => 'tl_member_group.name',
+			'eval'                    => array('mandatory'=>true, 'multiple'=>true),
+			'sql'                     => "blob NULL",
+			'relation'                => array('type'=>'hasMany', 'load'=>'lazy')
+		),
+		'allowComments' => array
+		(
+			'exclude'                 => true,
+			'filter'                  => true,
+			'inputType'               => 'checkbox',
+			'eval'                    => array('submitOnChange'=>true),
+			'sql'                     => "char(1) NOT NULL default ''"
+		),
+		'notify' => array
+		(
+			'exclude'                 => true,
+			'inputType'               => 'select',
+			'options'                 => array('notify_admin', 'notify_author', 'notify_both'),
+			'eval'                    => array('tl_class'=>'w50'),
+			'reference'               => &$GLOBALS['TL_LANG']['tl_podcast'],
+			'sql'                     => "varchar(16) NOT NULL default 'notify_admin'"
+		),
+		'sortOrder' => array
+		(
+			'exclude'                 => true,
+			'inputType'               => 'select',
+			'options'                 => array('ascending', 'descending'),
+			'reference'               => &$GLOBALS['TL_LANG']['MSC'],
+			'eval'                    => array('tl_class'=>'w50 clr'),
+			'sql'                     => "varchar(32) NOT NULL default 'ascending'"
+		),
+		'perPage' => array
+		(
+			'exclude'                 => true,
+			'inputType'               => 'text',
+			'eval'                    => array('rgxp'=>'natural', 'tl_class'=>'w50'),
+			'sql'                     => "smallint(5) unsigned NOT NULL default 0"
+		),
+		'moderate' => array
+		(
+			'exclude'                 => true,
+			'inputType'               => 'checkbox',
+			'eval'                    => array('tl_class'=>'w50'),
+			'sql'                     => "char(1) NOT NULL default ''"
+		),
+		'bbcode' => array
+		(
+			'exclude'                 => true,
+			'inputType'               => 'checkbox',
+			'eval'                    => array('tl_class'=>'w50'),
+			'sql'                     => "char(1) NOT NULL default ''"
+		),
+		'requireLogin' => array
+		(
+			'exclude'                 => true,
+			'inputType'               => 'checkbox',
+			'eval'                    => array('tl_class'=>'w50'),
+			'sql'                     => "char(1) NOT NULL default ''"
+		),
+		'disableCaptcha' => array
+		(
+			'exclude'                 => true,
+			'inputType'               => 'checkbox',
+			'eval'                    => array('tl_class'=>'w50'),
+			'sql'                     => "char(1) NOT NULL default ''"
+		)
+    )
+);
