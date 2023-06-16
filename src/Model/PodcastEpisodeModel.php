48f0d24c2fe9f48575d50938418a2eda10a4d018
Hamid Peywasti <abbaszadeh.h@gmail.com>
2023-06-16 14:32:31 +0330

    add new method

--- src/Model/PodcastEpisodeModel.php
+++ src/Model/PodcastEpisodeModel.php
@@ -14,9 +14,37 @@ declare(strict_types=1);
 
 namespace Respinar\ContaoPodcastBundle\Model;
 
+use Contao\Date;
 use Contao\Model;
 
 class PodcastEpisodeModel extends Model
 {
     protected static $strTable = 'tl_podcast_episode';
+
+    /**
+	 * Find published news items with the default redirect target by their parent ID
+	 *
+	 * @param integer $intPid     The news archive ID
+	 * @param array   $arrOptions An optional options array
+	 *
+	 * @return Collection|PodcastEpisodeModel[]|PodcastEpisodeModel|null A collection of models or null if there are no news
+	 */
+	public static function findPublishedDefaultByPid($intPid, array $arrOptions=array())
+	{
+		$t = static::$strTable;
+		$arrColumns = array("$t.pid=?");
+
+		if (!static::isPreviewMode($arrOptions))
+		{
+			$time = Date::floorToMinute();
+			$arrColumns[] = "$t.published=1 AND ($t.start='' OR $t.start<=$time) AND ($t.stop='' OR $t.stop>$time)";
+		}
+
+		if (!isset($arrOptions['order']))
+		{
+			$arrOptions['order'] = "$t.date DESC";
+		}
+
+		return static::findBy($arrColumns, array($intPid), $arrOptions);
+	}
 }
