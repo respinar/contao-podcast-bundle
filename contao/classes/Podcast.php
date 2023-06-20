b0d87cae7d8bd73d4cef27fb9652619d0a191e8e
Hamid A. Peywasti <abbaszadeh.h@gmail.com>
2023-06-20 22:08:00 +0330

    Rename to Podcast.php

---
+++ contao/classes/Podcast.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of Contao.
+ *
+ * (c) Leo Feyer
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+namespace Respinar\PodcastBundle;
+
+use Contao\Frontend;
+
+/**
+ * Provide methods regarding calendars.
+ */
+class Podcast extends Frontend
+{
+
+}
+
+class_alias(Podcast::class, 'Podcast');
