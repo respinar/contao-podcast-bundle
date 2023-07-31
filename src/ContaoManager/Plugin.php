4a02d36bc713374b137630456b2f87aacb616c3d
Hamid Peywasti <hamid@respinar.com>
2023-07-31 23:54:41 +0330

    use namespaces

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -19,14 +19,16 @@ use Contao\ManagerPlugin\Routing\RoutingPluginInterface;
 use Symfony\Component\Config\Loader\LoaderResolverInterface;
 use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\Routing\RouteCollection;
+use Contao\CoreBundle\ContaoCoreBundle;
+use Respinar\PodcastBundle\RespinarPodcastBundle;
 
 class Plugin implements BundlePluginInterface, RoutingPluginInterface
 {
     public function getBundles(ParserInterface $parser): array
     {
         return [
-            BundleConfig::create('Respinar\PodcastBundle\RespinarContaoPodcastBundle')
-                ->setLoadAfter(['Contao\CoreBundle\ContaoCoreBundle']),
+            BundleConfig::create(RespinarPodcastBundle::class)
+                ->setLoadAfter([ContaoCoreBundle::class]),
         ];
     }
 
