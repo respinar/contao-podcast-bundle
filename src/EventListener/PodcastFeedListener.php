50f2f5af3cfffa4ba76f6f00433f5eac90d2619e
Hamid Peywasti <hamid@respinar.com>
2023-07-31 23:51:04 +0330

    Update License notices

--- src/EventListener/PodcastFeedListener.php
+++ src/EventListener/PodcastFeedListener.php
@@ -2,6 +2,14 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of Contao Simple Podcast.
+ *
+ * (c) Hamid Peywasti 2023 <hamid@respinar.com>
+ *
+ * @license MIT
+ */
+
 namespace Respinar\PodcastBundle\EventListener;
 
 use Contao\CoreBundle\Cache\EntityCacheTags;
