18ca59f2e91c98c3223c5bc846a0f0f3ef7d5fea
Hamid Peywasti <hamid@respinar.com>
2023-09-09 23:44:32 +0330

    Update License notice

--- src/Controller/Podcast.php
+++ src/Controller/Podcast.php
@@ -2,6 +2,14 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of Contao Simple Podcast.
+ *
+ * (c) Hamid Peywasti 2023 <hamid@respinar.com>
+ *
+ * @license MIT
+ */
+
 namespace Respinar\PodcastBundle\Controller;
 
 use Contao\System;
