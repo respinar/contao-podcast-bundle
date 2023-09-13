82707458d0253ce1a4c77a064857d93179ec2184
Hamid Peywasti <hamid@respinar.com>
2023-09-13 23:53:15 +0330

    add new method

--- src/Model/EpisodeModel.php
+++ src/Model/EpisodeModel.php
@@ -19,18 +19,47 @@ class EpisodeModel extends Model
 {
     protected static $strTable = 'tl_podcast_episode';
 
-    /**
-	 * Find published news items with the default redirect target by their parent ID
+	/**
+	 * Find a published episode from one or more podcast channel by its ID or alias
 	 *
-	 * @param integer $intPid     The news archive ID
+	 * @param mixed $varId      The numeric ID or alias name
+	 * @param array $arrPids    An array of parent IDs
+	 * @param array $arrOptions An optional options array
+	 *
+	 * @return EpisodeModel|null The model or null if there are no episode
+	 */
+	public static function findPublishedByParentAndIdOrAlias($varId, $arrPids, array $arrOptions=array())
+	{
+		if (empty($arrPids) || !\is_array($arrPids))
+		{
+			return null;
+		}
+
+		$t = static::$strTable;
+		$arrColumns = !preg_match('/^[1-9]\d*$/', $varId) ? array("BINARY $t.alias=?") : array("$t.id=?");
+		$arrColumns[] = "$t.pid IN(" . implode(',', array_map('\intval', $arrPids)) . ")";
+
+		if (!static::isPreviewMode($arrOptions))
+		{
+			$time = Date::floorToMinute();
+			$arrColumns[] = "$t.published=1 AND ($t.start='' OR $t.start<=$time) AND ($t.stop='' OR $t.stop>$time)";
+		}
+
+		return static::findOneBy($arrColumns, array($varId), $arrOptions);
+	}
+
+	/**
+	 * Find published episodes with the default redirect target by their parent ID
+	 *
+	 * @param integer $intPid     The podcast channel ID
 	 * @param array   $arrOptions An optional options array
 	 *
-	 * @return Collection|PodcastEpisodeModel[]|PodcastEpisodeModel|null A collection of models or null if there are no news
+	 * @return Collection<EpisodeModel>|EpisodeModel[]|null A collection of models or null if there are no episode
 	 */
 	public static function findPublishedDefaultByPid($intPid, array $arrOptions=array())
 	{
 		$t = static::$strTable;
-		$arrColumns = array("$t.pid=?");
+		$arrColumns = array("$t.pid=? AND $t.source='default'");
 
 		if (!static::isPreviewMode($arrOptions))
 		{
@@ -45,4 +74,5 @@ class EpisodeModel extends Model
 
 		return static::findBy($arrColumns, array($intPid), $arrOptions);
 	}
+
 }
