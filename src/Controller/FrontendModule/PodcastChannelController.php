f6a34680d51a210983026c8c809cab5e4ef6e5c9
Hamid Peywasti <hamid@respinar.com>
2023-09-16 21:13:18 +0330

    add sorting anf pagination functionallty

--- src/Controller/FrontendModule/PodcastChannelController.php
+++ src/Controller/FrontendModule/PodcastChannelController.php
@@ -17,7 +17,9 @@ use Contao\CoreBundle\DependencyInjection\Attribute\AsFrontendModule;
 use Contao\ModuleModel;
 use Contao\Template;
 use Contao\Input;
+use Contao\Pagination;
 use Contao\StringUtil;
+use Contao\Config;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -37,15 +39,129 @@ class PodcastChannelController extends AbstractFrontendModuleController
     {
         $page = $this->getPageModel();
 
-        //$objChannel = ChannelModel::findOneBy('id', StringUtil::deserialize($model->podcast)[0]);
+        $template->empty = $GLOBALS['TL_LANG']['MSC']['emptyChannel'];
 
         if (Podcast::isProtected($model->podcast_channel)) {
             return $template->getResponse();
         }
 
-        $objEpisodes = EpisodeModel::findBy('pid', $model->podcast_channel);
+        $objChannel = ChannelModel::findOneBy('id', $model->podcast_channel);
 
-        $template->arrEpisodes = Podcast::parseEpisodes($objEpisodes, $model, $page);
+        // No Podcast Channel
+		if (empty($objChannel))
+		{
+			return  $template->getResponse();
+		}
+
+        $offset = intval($model->skipFirst);
+		$limit = null;
+
+		// Maximum number of items
+		if ($model->numberOfItems > 0)
+		{
+			$limit = $model->numberOfItems;
+		}
+
+		// Handle featured product
+		if ($model->podcast_featured == 'featured')
+		{
+			$blnFeatured = true;
+		}
+		elseif ($model->podcast_featured == 'unfeatured')
+		{
+			$blnFeatured = false;
+		}
+		else
+		{
+			$blnFeatured = null;
+		}
+
+		$template->episodes = array();
+
+		$intTotal = EpisodeModel::countPublishedByPids([$model->podcast_channel], $blnFeatured);
+
+		if ($intTotal < 1)
+		{
+			return $template->getResponse();
+		}
+
+		$total = $intTotal - $offset;
+
+		// Split the results
+		if ($model->perPage > 0 && (!isset($limit) || $model->numberOfItems > $model->perPage))
+		{
+			// Adjust the overall limit
+			if (isset($limit))
+			{
+				$total = min($limit, $total);
+			}
+
+			// Get the current page
+			$id = 'page_n' . $model->id;
+			$page = Input::get($id) ?: 1;
+
+			// Do not index or cache the page if the page number is outside the range
+			if ($page < 1 || $page > max(ceil($total/$model->perPage), 1))
+			{
+				global $objPage;
+				$objPage->noSearch = 1;
+				$objPage->cache = 0;
+
+				// Send a 404 header
+				header('HTTP/1.1 404 Not Found');
+				return $template->getResponse();
+			}
+
+			// Set limit and offset
+			$limit = $model->perPage;
+			$offset += (max($page, 1) - 1) * $model->perPage;
+			$skip = intval($model->skipFirst);
+
+			// Overall limit
+			if ($offset + $limit > $total + $skip)
+			{
+				$limit = $total + $skip - $offset;
+			}
+
+			// Add the pagination menu
+			$objPagination = new Pagination($total, $model->perPage, Config::get('maxPaginationLinks'), $id);
+			$template->pagination = $objPagination->generate("\n  ");
+		}
+
+		$arrOptions = array();
+		if ($model->podcast_sortBy)
+		{
+			switch ($model->podcast_sortBy)
+			{
+				case 'number_asc':
+					$arrOptions['order'] = "episodeNumber ASC";
+					break;
+				case 'number_desc':
+					$arrOptions['order'] = "episodeNumber DESC";
+					break;
+				case 'date_asc':
+					$arrOptions['order'] = "date ASC";
+					break;
+				case 'date_desc':
+					$arrOptions['order'] = "date DESC";
+					break;
+			}
+		}
+
+
+        // Get the items
+		if (isset($limit))
+		{
+			$objEpisodes = EpisodeModel::findPublishedByPids([$model->podcast_channel], $blnFeatured, $limit, $offset, $arrOptions);
+		}
+		else
+		{
+			$objEpisodes = EpisodeModel::findPublishedByPids([$model->podcast_channel], $blnFeatured, 0, $offset, $arrOptions);
+		}
+
+        //$objEpisodes = EpisodeModel::findBy('pid', $model->podcast_channel);
+
+        $template->episodes = Podcast::parseEpisodes($objEpisodes, $model, $page);
 
         return $template->getResponse();
     }
