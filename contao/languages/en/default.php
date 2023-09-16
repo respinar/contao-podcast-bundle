5b96c4122cd1560693b59022ab7c64e03180f17d
Hamid Peywasti <hamid@respinar.com>
2023-09-16 21:44:44 +0330

    add new strings

--- contao/languages/en/default.php
+++ contao/languages/en/default.php
@@ -10,18 +10,33 @@ declare(strict_types=1);
  * @license MIT
  */
 
-use Respinar\PodcastBundle\Controller\ContentElement\PodcastEpisodeController;
+use Respinar\PodcastBundle\Controller\ContentElement\PodcastController;
+use Respinar\PodcastBundle\Controller\FrontendModule\PodcastChannelController;
+use Respinar\PodcastBundle\Controller\FrontendModule\PodcastEpisodeController;
+
+/**
+ * Backend modules
+ */
+$GLOBALS['TL_LANG']['MOD']['podcasts'] = ['Podcasts', 'Manage Podcasts'];
 
 /**
  * Content element
  */
-$GLOBALS['TL_LANG']['CTE']['podcast_elements'] = 'Podcasts';
-$GLOBALS['TL_LANG']['CTE'][PodcastEpisodeController::TYPE] = ['Podcast', 'Add a podcast'];
+$GLOBALS['TL_LANG']['CTE'][PodcastController::TYPE] = ['Podcast', 'Add an Episode of podcast'];
+
+/**
+ * Frontend modules
+ */
+$GLOBALS['TL_LANG']['FMD']['podcast_modules'] = 'Podacsts';
+$GLOBALS['TL_LANG']['FMD'][PodcastEpisodeController::TYPE] = ['Episode reader', 'Show deatil of an Episode'];
+$GLOBALS['TL_LANG']['FMD'][PodcastChannelController::TYPE] = ['Channel list', 'Show Episodes of a Channel'];
 
 /**
  * Miscellaneous
  */
-//$GLOBALS['TL_LANG']['MSC'][''] = '';
+$GLOBALS['TL_LANG']['MSC']['notExist'] = 'There is a problem!';
+$GLOBALS['TL_LANG']['MSC']['accessError'] = 'You do not have access to this channel!';
+$GLOBALS['TL_LANG']['MSC']['emptyChannel'] = 'There is no episode in this channel!';
 
 /**
  * Errors
