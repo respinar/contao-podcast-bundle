d0b66a769434ed7757489ab56e08efeb106b4127
Hamid Peywasti <hamid@respinar.com>
2023-09-16 21:18:16 +0330

    add new methods

--- src/Model/EpisodeModel.php
+++ src/Model/EpisodeModel.php
@@ -75,4 +75,89 @@ class EpisodeModel extends Model
 		return static::findBy($arrColumns, array($intPid), $arrOptions);
 	}
 
+	/**
+	 * Count published product items by their parent ID
+	 *
+	 * @param array   $arrPids     An array of product catalogs IDs
+	 * @param boolean $blnFeatured If true, return only featured product, if false, return only unfeatured product
+	 * @param array   $arrOptions  An optional options array
+	 *
+	 * @return integer The number of product items
+	 */
+	public static function countPublishedByPid($pid, $blnFeatured=null, array $arrOptions=array())
+	{
+		if (empty($pid))
+		{
+			return 0;
+		}
+
+		$t = static::$strTable;
+		$arrColumns = array("$t.pid = $pid");
+
+		if ($blnFeatured === true)
+		{
+			$arrColumns[] = "$t.featured=1";
+		}
+		elseif ($blnFeatured === false)
+		{
+			$arrColumns[] = "$t.featured=''";
+		}
+
+		if (!BE_USER_LOGGED_IN)
+		{
+			$time = time();
+			$arrColumns[] = "($t.start='' OR $t.start<$time) AND ($t.stop='' OR $t.stop>$time) AND $t.published=1";
+		}
+
+		return static::countBy($arrColumns, null, $arrOptions);
+	}
+
+	/**
+	 * Find published product items by their parent ID
+	 *
+	 * @param array   $arrPids     An array of product catalogs IDs
+	 * @param boolean $blnFeatured If true, return only featured product, if false, return only unfeatured product
+	 * @param integer $intLimit    An optional limit
+	 * @param integer $intOffset   An optional offset
+	 * @param array   $arrOptions  An optional options array
+	 *
+	 * @return \Model\Collection|null A collection of models or null if there are no product
+	 */
+	public static function findPublishedByPid($pid, $blnFeatured=null, $intLimit=0, $intOffset=0, array $arrOptions=array())
+	{
+		if (empty($pid))
+		{
+			return null;
+		}
+
+		$t = static::$strTable;
+		$arrColumns = array("$t.pid = $pid");
+
+		if ($blnFeatured === true)
+		{
+			$arrColumns[] = "$t.featured=1";
+		}
+		elseif ($blnFeatured === false)
+		{
+			$arrColumns[] = "$t.featured=''";
+		}
+
+		// Never return unpublished elements in the back end, so they don't end up in the RSS feed
+		if (!BE_USER_LOGGED_IN || TL_MODE == 'BE')
+		{
+			$time = time();
+			$arrColumns[] = "($t.start='' OR $t.start<$time) AND ($t.stop='' OR $t.stop>$time) AND $t.published=1";
+		}
+
+		if (!isset($arrOptions['order']))
+		{
+			$arrOptions['order']  = "$t.date DESC";
+		}
+
+		$arrOptions['limit']  = $intLimit;
+		$arrOptions['offset'] = $intOffset;
+
+		return static::findBy($arrColumns, null, $arrOptions);
+	}
+
 }
