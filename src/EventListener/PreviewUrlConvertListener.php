cc540938e887c3bafe277d551dfcc9c373620103
Hamid Peywasti <hamid@respinar.com>
2023-10-17 16:45:49 +0330

    add event listener for convert Preview Url

---
+++ src/EventListener/PreviewUrlConvertListener.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao Podcast Bundle.
+ *
+ * (c) Hamid Peywasti 2023 <hamid@respinar.com>
+ *
+ * @license MIT
+ */
+
+namespace Respinar\PodcastBundle\EventListener;
+
+use Contao\CoreBundle\Event\PreviewUrlConvertEvent;
+use Contao\CoreBundle\Framework\ContaoFramework;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
+use Respinar\PodcastBundle\Model\EpisodeModel;
+
+use Respinar\PodcastBundle\Controller\Podcast;
+use Spatie\SchemaOrg\Episode;
+
+#[AsEventListener('contao.preview_url_convert')]
+class PreviewUrlConvertListener
+{
+    private ContaoFramework  $framework;
+
+    public function __construct(ContaoFramework $framework)
+    {
+        $this->framework = $framework;
+    }
+
+    public function __invoke(PreviewUrlConvertEvent $event): void
+    {
+        // Do something
+        if (!$this->framework->isInitialized()) {
+            return;
+        }
+
+        if (null === ($podcast = $this->getPodcastModel($event->getRequest()))) {
+            return;
+        }
+
+        $event->setUrl($this->framework->getAdapter(Podcast::class)->generateEpisodeUrl($podcast, false, true));
+
+    }
+
+    private function getPodcastModel(Request $request): ?EpisodeModel
+    {
+        if (!$request->query->has('podcast')) {
+            return null;
+        }
+
+        return $this->framework->getAdapter(EpisodeModel::class)->findByPk($request->query->get('podcast'));
+    }
+}
