578990e5a4f137897b3d7abc56a0a9625aa7b01a
Hamid Peywasti <hamid@respinar.com>
2023-10-23 16:09:26 +0330

    rename namespace

--- contao/classes/Podcast.php
+++ contao/classes/Podcast.php
@@ -10,7 +10,7 @@ declare(strict_types=1);
  * @license MIT
  */
 
-namespace Respinar\PodcastBundle\Controller;
+namespace Respinar\PodcastBundle;
 
 use Contao\System;
 use Contao\FrontendTemplate;
