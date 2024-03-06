3ad29dabe38d145ab0b8abb4edbce48a47f28534
Hamid Peywasti <hamid@respinar.com>
2024-03-06 19:16:37 +0330

    use type declarations

--- contao/models/EpisodeModel.php
+++ contao/models/EpisodeModel.php
@@ -98,16 +98,8 @@ class EpisodeModel extends Model
 
 	/**
 	 * Find published product items by their parent ID
-	 *
-	 * @param array   $arrPids     An array of product catalogs IDs
-	 * @param boolean $blnFeatured If true, return only featured product, if false, return only unfeatured product
-	 * @param integer $intLimit    An optional limit
-	 * @param integer $intOffset   An optional offset
-	 * @param array   $arrOptions  An optional options array
-	 *
-	 * @return \Model\Collection|null A collection of models or null if there are no product
 	 */
-	public static function findPublishedByPid($pid, $blnFeatured=null, $intLimit=0, $intOffset=0, array $arrOptions=array())
+	public static function findPublishedByPid(int $pid, bool $blnFeatured=null, int $intLimit=0, int $intOffset=0, array $arrOptions=array()): Collection|EpisodeModel|null
 	{
 		if (empty($pid))
 		{
