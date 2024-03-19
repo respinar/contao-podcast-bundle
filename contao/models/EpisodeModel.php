c3a070f5341da5c751e00d8cf39639704f31a8c5
Hamid Peywasti <hamid@respinar.com>
2024-03-19 12:31:39 +0330

    make compatiable with contao ^5.3

--- contao/models/EpisodeModel.php
+++ contao/models/EpisodeModel.php
@@ -31,7 +31,7 @@ class EpisodeModel extends Model
 		}
 
 		$t = static::$strTable;
-		$arrColumns = !preg_match('/^[1-9]\d*$/', $varId) ? array("BINARY $t.alias=?") : array("$t.id=?");
+		$arrColumns = !preg_match('/^[1-9]\d*$/', $varId) ? array("CAST($t.alias AS BINARY)=?") : array("$t.id=?");
 		$arrColumns[] = "$t.pid IN(" . implode(',', array_map('\intval', $arrPids)) . ")";
 
 		if (!static::isPreviewMode($arrOptions))
