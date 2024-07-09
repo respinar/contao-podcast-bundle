87ddb38e7735de19435def89cf3d5fc05455ab2e
Hamid Peywasti <hamid@respinar.com>
2024-07-09 22:21:48 +0330

    rename namspace of bundle

--- src/RespinarPodcastBundle.php
+++ src/RespinarPodcastBundle.php
@@ -10,7 +10,7 @@ declare(strict_types=1);
  * @license MIT
  */
 
-namespace Respinar\PodcastBundle;
+namespace Respinar\ContaoPodcastBundle;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
